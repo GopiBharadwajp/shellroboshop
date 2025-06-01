edea6a803a0e6961444f940653c0d1a07c790e87
gopi bharadwaj <gopipathapalli@gmail.com>
2025-06-01 08:05:23 +0530

    code added

--- roboshop.sh
+++ roboshop.sh
@@ -6,27 +6,72 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z0095891220EQ8FJXWFW1"
 DOMAINNAME="gorobo.site"
 
-for instance in "${INSTANCES[@]}"
+if [ $# -gt 0 ]; then
+  instances=("$@")
+else
+  instances=("${INSTANCES[@]}")
+fi
+
+for instance in "${instances[@]}"
 do
+    echo "Creating instance for: $instance"
+
     INSTANCE_ID=$(aws ec2 run-instances \
-        --image-id $AMI_ID \
-        --instance-type t2.micro \
-        --security-group-ids $SG_ID \
+        --image-id "$AMI_ID" \
+        --instance-type t3.micro \
+        --security-group-ids "$SG_ID" \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
         --query "Instances[0].InstanceId" \
         --output text)
 
+    echo "Launched instance ID: $INSTANCE_ID"
+    
+    # Wait for instance to be in running state
+    aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
+
     if [ "$instance" != "frontend" ]; then
         IP=$(aws ec2 describe-instances \
-            --instance-ids $INSTANCE_ID \
+            --instance-ids "$INSTANCE_ID" \
             --query "Reservations[0].Instances[0].PrivateIpAddress" \
             --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances \
-            --instance-ids $INSTANCE_ID \
+            --instance-ids "$INSTANCE_ID" \
             --query "Reservations[0].Instances[0].PublicIpAddress" \
             --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
 
     echo "$instance IP address: $IP"
-done
+
+    # Create JSON payload dynamically
+    CHANGE_BATCH=$(cat <<EOF
+{
+    "Comment": "Creating or Updating a record set for $instance",
+    "Changes": [
+        {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "$RECORD_NAME",
+                "Type": "A",
+                "TTL": 300,
+                "ResourceRecords": [
+                    {
+                        "Value": "$IP"
+                    }
+                ]
+            }
+        }
+    ]
+}
+EOF
+)
+
+   
+
+
+
+
+
+
