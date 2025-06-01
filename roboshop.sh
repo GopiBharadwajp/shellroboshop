38b6d9d8fdaacf3c921baf6654338458a3f331d0
gopi bharadwaj <gopipathapalli@gmail.com>
2025-06-01 08:49:46 +0530

    code added

--- roboshop.sh
+++ roboshop.sh
@@ -6,78 +6,37 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z0095891220EQ8FJXWFW1"
 DOMAINNAME="gorobo.site"
 
-if [ $# -gt 0 ]; then
-  instances=("$@")
-else
-  instances=("${INSTANCES[@]}")
-fi
 
-for instance in "${instances[@]}"
+
+#for instance in ${INSTANCES[@]}
+for instance in $@
 do
-    echo "Creating instance for: $instance"
-
-    INSTANCE_ID=$(aws ec2 run-instances \
-        --image-id "$AMI_ID" \
-        --instance-type t3.micro \
-        --security-group-ids "$SG_ID" \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-        --query "Instances[0].InstanceId" \
-        --output text)
-
-    echo "Launched instance ID: $INSTANCE_ID"
-    
-    # Wait for instance to be in running state
-    aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
-
-    if [ "$instance" != "frontend" ]; then
-        IP=$(aws ec2 describe-instances \
-            --instance-ids "$INSTANCE_ID" \
-            --query "Reservations[0].Instances[0].PrivateIpAddress" \
-            --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
     else
-        IP=$(aws ec2 describe-instances \
-            --instance-ids "$INSTANCE_ID" \
-            --query "Reservations[0].Instances[0].PublicIpAddress" \
-            --output text)
-        RECORD_NAME="${instance}.${DOMAIN_NAME}"
-
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
-
     echo "$instance IP address: $IP"
 
-    # Create JSON payload dynamically
-    CHANGE_BATCH=$(cat <<EOF
-CHANGE_BATCH=$(cat <<EOF
-{
-    "Comment": "Creating or Updating a record set for $instance",
-    "Changes": [
-        {
-            "Action": "UPSERT",
-            "ResourceRecordSet": {
-                "Name": "$RECORD_NAME",
-                "Type": "A",
-                "TTL": 300,
-                "ResourceRecords": [
-                    {
-                        "Value": "$IP"
-                    }
-                ]
-            }
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
         }
-    ]
-}
-EOF
-)
-
-
-   
-
-
-   aws route53 change-resource-record-sets \
-        --hosted-zone-id "$ZONE_ID" \
-        --change-batch "$CHANGE_BATCH"
+        }]
+    }'
 done
-
-
-
